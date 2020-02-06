73920bffe3a7119723c90646726bed1435e64a69
dagute <1271@holbertonschool.com>
2020-02-06 03:01:17 +0000

    Quick Sort - Hoare Partition scheme

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,80 @@
+#include "sort.h"
+/**
+ * quick_sort2 - sorts an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: pointer
+ * @beg: beginning of the array
+ * @end: end of the array
+ * @size: size
+ * Return: nothing
+ */
+void quick_sort2(int *array, int beg, int end, size_t size)
+{
+	int pos;
+
+	if (beg < end)
+	{
+		pos = part_hoare(array, beg, end, size);
+		quick_sort2(array, beg, pos - 1, size);
+		quick_sort2(array, pos, end, size);
+	}
+}
+/**
+ * part_hoare - rearrange the array
+ * @array: array
+ * @beg: beginning of the array
+ * @end: end of the array
+ * @size: size
+ * Return: index
+ */
+int part_hoare(int *array, int beg, int end, size_t size)
+{
+	int j = end;
+	int pivot = array[end];
+	int i = beg - 1;
+
+	do {
+		i++;
+		while (array[i] < pivot)
+		{
+			i++;
+		}
+		while (array[j] > pivot)
+		{
+			j--;
+		}
+		if (i < j)
+		{
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	} while (i < j);
+	return (i);
+}
+/**
+ * swap - swap two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * quick_sort_hoare - sorts an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: array
+ * @size: size of the array
+ * Return: nothing
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort2(array, 0, size - 1, size);
+}
